7be38f39cd96e5a4390c4ef05736d975bae8daaf
wujinhua <1938054289@qq.com>
2022-06-28 16:00:35 +0800

    feat: error 404页面

--- src/app.js
+++ src/app.js
@@ -6,11 +6,20 @@ const onerror = require('koa-onerror')
 const bodyparser = require('koa-bodyparser')
 const logger = require('koa-logger')
 
+const { isProd } = require('./utils/env')
+
 const index = require('./routes/index')
 const users = require('./routes/users')
+const errorViewRouter = require('./routes/view/error')
 
 // error handler
-onerror(app)
+let onerrorConf = {}
+if (isProd) {
+  onerrorConf = {
+    redirect: '/error',
+  }
+}
+onerror(app, onerrorConf)
 
 // middlewares
 app.use(
@@ -39,6 +48,8 @@ app.use(
 // routes
 app.use(index.routes(), index.allowedMethods())
 app.use(users.routes(), users.allowedMethods())
+
+app.use(errorViewRouter.routes(), errorViewRouter.allowedMethods())
 
 // error-handling
 app.on('error', (err, ctx) => {
